b6c468c6138bf37cba7504b2ca2c9da9cd05de77
craig <craigya89@gmail.com>
2020-01-27 20:04:14 +0800

    Setup Bootstrap in webpack - Yarn add Bootstrap - require Bootstrap in packs / application.js - import Bootstrap in stylesheets / application.scss - modify view index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def user_signed_in
     if user_signed_in?
-      link_to(current_user.email, edit_user_registration_path) +
-        link_to('登出', destroy_user_session_path, method: 'delete')
+      link_to(current_user.email, edit_user_registration_path, class: 'btn btn-light') +
+        link_to('登出', destroy_user_session_path, method: 'delete', class: 'btn btn-secondary')
     else
-      link_to('註冊', new_user_registration_path) +
-        link_to('登入', new_user_session_path)
+      link_to('註冊', new_user_registration_path, class: 'btn btn-success') +
+        link_to('登入', new_user_session_path, class: 'btn btn-secondary')
     end
   end
 end
