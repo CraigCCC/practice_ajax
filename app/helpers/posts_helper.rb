f86fcd2879cc0024af00b8c70ccad0defb97b4d9
craig <craigya89@gmail.com>
2020-01-27 21:39:45 +0800

    Ajax-destroy - remove posts_controller action: destroy - add remote: true in posts_helper , that can enable ajax - new destroy.js.erb

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper # :nodoc:
   def current_user_is_post_user_that_can_delete(post)
-    link_to('Delete', post_path(post), method: 'delete', class: 'btn btn-danger') if current_user && (post.user == current_user)
+    link_to('Delete', post_path(post), method: 'delete', remote: true, class: 'btn btn-danger') if current_user && (post.user == current_user)
   end
 end
