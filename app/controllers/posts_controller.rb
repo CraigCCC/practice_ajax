fd2d4cb8ee835fab12747c59a0bd1056245d6ea7
craig <craigya89@gmail.com>
2020-01-29 19:03:34 +0800

    User like feat

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,20 @@ class PostsController < ApplicationController # :nodoc:
     @post.destroy
   end
 
+  def like
+    @post = Post.find(params[:id])
+    Like.create(user: current_user, post: @post) if !@post.find_like(current_user)
+
+    redirect_to posts_path
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    like = @post.find_like(current_user)
+    like.destroy
+
+    redirect_to posts_path
+  end
   private
 
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ApplicationRecord # :nodoc:
   # association
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :likes_users, through: :likes, source: :user
 
   # validation
   validates :content, presence: true
+
+  def find_like(user)
+    self.likes.where( user_id: user.id ).first
+  end
 end
